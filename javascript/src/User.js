b25cdb7f036022000f474874b08d3ca37ea2ff9c
robmoo <robert.moorcroft@coop.co.uk>
2022-05-27 15:00:29 +0100

    Convert TS methods to JS for JS exercise in User class

--- javascript/src/User.js
+++ javascript/src/User.js
@@ -1,5 +1,22 @@
 "use strict";
 
 module.exports = class User {
+  trips = [];
+  friends = [];
 
-}
+  getFriends() {
+    return this.friends;
+  }
+
+  addFriend(user) {
+    this.friends.push(user);
+  }
+
+  addTrip(trip) {
+    this.trips.push(trip);
+  }
+
+  getTrips() {
+    return this.trips;
+  }
+};
